198124c833fdb45e66064b3e6cd1c06e62f3a24c
Arif Rahman <gm.arifrahman@gmail.com>
2025-01-20 23:34:30 +0700

    competition

--- src/app/competition/index.jsx
+++ src/app/competition/index.jsx
@@ -68,26 +68,47 @@ const chartConfig = {
 export default function Competition() {
   const dataHeader = [
     {
-      id: 1,
-      title: "Soeratin U13",
+      id: 17,
+      title: "Piala Soeratin U13",
       totalClubs: 166,
       totalPlayers: 2502,
     },
     {
-      id: 2,
-      title: "Soeratin U15",
+      id: 15,
+      title: "Piala Soeratin U15",
       totalClubs: 166,
       totalPlayers: 2502,
     },
     {
-      id: 3,
+      id: 14,
       title: "Soeratin U17",
       totalClubs: 166,
       totalPlayers: 2502,
     },
     {
-      id: 4,
+      id: 13,
       title: "Liga4",
+      totalClubs: 166,
+      totalPlayers: 2502,
+    },
+
+    {
+      id: 31,
+      title: "Piala Pertiwi U15",
+      totalClubs: 166,
+      totalPlayers: 2502,
+    },
+
+    {
+      id: 30,
+      title: "Piala Pertiwi U17",
+      totalClubs: 166,
+      totalPlayers: 2502,
+    },
+
+    {
+      id: 21,
+      title: "Piala Pertiwi",
       totalClubs: 166,
       totalPlayers: 2502,
     },
@@ -134,56 +155,22 @@ export default function Competition() {
     <div>
       <div className="bg-[#7E0000] absolute h-[60vh] w-full z-10"></div>
       <NavBar  bgColor="#FFFFFF" selectedTextColor="#7E0000" secondaryTextColor="#C6C6C6" />
-      <div className="z-20 relative container-pssi">
-        <h2 className="text-[#A54D4D] text-3xl font-bold">Competition</h2>
-        <p className="text-sm text-neutral-400 mt-2">
-          An Indonesian competition is an organized event where participants
-          compete in various fields, such as sports, arts, or academics, at
-          regional, national, or international levels.
-        </p>
-
-        <div className="flex flex-row mt-5 bg-white p-5">
-          {dataHeader.map((item) => (
-            <div
-              key={item.id}
-              onClick={() => setActiveCard(item.id)}
-              style={{ cursor: "pointer" }}
-              className={`${
-                activeCard === item.id
-                  ? "bg-gradient-to-r from-[#92D4F8] from-10% via-[#92D4F8] via-30% via-[#006FFF] via-50% via-[#16A34A] via-70% to-[#81F6E8] to-90%"
-                  : "bg-white"
-              } rounded-xl shadow-lg p-3 w-full ml-3`}
-            >
-              <div className="flex justify-between">
-                <span className="text-gray-700 text-[18px] text-lg font-bold">
-                  {item.title}
-                </span>
-              </div>
-              {activeCard === item.id && (
-                <>
-                  <div className="flex justify-between">
-                    <span className="text-gray-700 text-[12px]">Total Club</span>
-                    <span className="text-[#006FFF] font-medium text-[12px]">
-                      {item.totalClubs}
-                    </span>
-                  </div>
-                  <div className="flex justify-between">
-                    <span className="text-gray-700 text-[12px]">
-                      Total Players
-                    </span>
-                    <span className="text-[#16A34A] font-medium text-[12px]">
-                      {item.totalPlayers}
-                    </span>
-                  </div>
-                </>
-              )}
-            </div>
-          ))}
+      <div className="relative flex flex-col gap-5 container-pssi mx-4 z-20  ">
+        
+        <div>
+          <h2 className="text-[#A54D4D] text-3xl font-bold">Competition</h2>
+          <p className="text-sm text-neutral-400 ">
+            An Indonesian competition is an organized event where participants
+            compete in various fields, such as sports, arts, or academics, at
+            regional, national, or international levels.
+          </p>
         </div>
 
-        <div className="flex flex-row mt-5 bg-white p-5">
+        
+
+        <div className="flex flex-row mt-2 rounded-lg border bg-white p-0">
           <div className="w-full">
-            <h3 className="font-semibold">Soeratin U13</h3>
+            {/* <h3 className="font-semibold">Soeratin U13</h3> */}
             {/* <Card>
               <CardContent>
                 <ChartContainer config={chartConfig}>
@@ -227,7 +214,59 @@ export default function Competition() {
               </CardContent>
             </Card> */}
 
-            <div className="flex flex-col mt-3">
+            <div className="flex flex-row gap-4 border-b px-4 py-3">
+              {dataHeader.map((item) => (
+                <div
+                  key={item.id}
+                  onClick={() => setActiveCard(item.id)}
+                  style={{ cursor: "pointer" }}
+                  className={`${
+                    activeCard === item.id
+                      ? "bg-[#7E0000] text-white"
+                      : "bg-slate-100"
+                  } rounded-lg  p-3 w-full relative`}
+                >
+                  
+                  <div className="flex mb-2 z-20 relative justify-between">
+                    <span className="  text-sm font-bold">
+                      {item.title}
+                    </span>
+                  </div>
+                  {activeCard === item.id && (
+                    <>
+                    <div className="z-20 relative">
+                      <div className="flex text-xs justify-between">
+                        <span className="">Total Club</span>
+                        <span className=" font-medium ">
+                          {item.totalClubs}
+                        </span>
+                      </div>
+                      <div className="flex text-xs  justify-between">
+                        <span className=" ">
+                          Total Players
+                        </span>
+                        <span className="font-medium ">
+                          {item.totalPlayers}
+                        </span>
+                      </div>
+                    </div>
+                  
+
+                    <div className="absolute z-10 bottom-[-1px] right-1">
+                      <svg width="34" height="34" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M8.67 14H4C2.9 14 2 14.9 2 16V22H8.67V14Z" stroke="#9A5250" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                      <path d="M13.33 10H10.66C9.56003 10 8.66003 10.9 8.66003 12V22H15.33V12C15.33 10.9 14.44 10 13.33 10Z" stroke="#9A5250" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                      <path d="M20 17H15.33V22H22V19C22 17.9 21.1 17 20 17Z" stroke="#9A5250" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                      <path d="M12.52 2.07007L13.05 3.13006C13.12 3.28006 13.31 3.42006 13.47 3.44006L14.43 3.60007C15.04 3.70007 15.19 4.15005 14.75 4.58005L14 5.33005C13.87 5.46005 13.8 5.70006 13.84 5.87006L14.05 6.79007C14.22 7.52007 13.83 7.80007 13.19 7.42007L12.29 6.89007C12.13 6.79007 11.86 6.79007 11.7 6.89007L10.8 7.42007C10.16 7.80007 9.76998 7.52007 9.93998 6.79007L10.15 5.87006C10.19 5.70006 10.12 5.45005 9.98999 5.33005L9.24999 4.59006C8.80999 4.15006 8.94999 3.71005 9.56999 3.61005L10.53 3.45007C10.69 3.42007 10.88 3.28007 10.95 3.14007L11.48 2.08005C11.77 1.50005 12.23 1.50007 12.52 2.07007Z" stroke="#9A5250" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                      </svg>
+                    </div>
+                    </>
+                  )}
+                </div>
+              ))}
+            </div>
+
+            <div className="flex flex-col mt-3 p-5">
               <div className="flex justify-end">
                 <Select>
                   <SelectTrigger className="w-[280px]">
@@ -252,6 +291,7 @@ export default function Competition() {
             <MapsChart dataMaps={dataMaps} />
           </div>
         </div>
+
         <div className="flex flex-row mt-5 bg-white p-5">
           <div className="w-full ml-5">
             <div className="text-black font-bold">Player List SoeratinU13</div>
