935b1c76be22b9d373d280221192e986e4d88a98
irham nurullah <nurullahirham@gmail.com>
2025-01-20 11:23:44 +0700

    add: direct dashboard if have token

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,12 +1,24 @@
-import { LoginForm } from "@/components/login-form"
+import { LoginForm } from '@/components/login-form';
+import { useEffect } from 'react';
+import sessions from '../../../utils/sessions';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = sessions.getSessionToken();
+
+    if (token) {
+      navigate('/dashboard');
+    }
+  }, [navigate]);
+
   return (
-    (<div
-      className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <LoginForm />
       </div>
-    </div>)
+    </div>
   );
 }
